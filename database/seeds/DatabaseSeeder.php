01dc6fe559529cd09d3ea974447f8f1e5f4027bb
Sai Thet Naung Oo <pyupyu140@gmail.com>
2021-10-25 22:36:08 +0630

    comment article

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        factory(App\Article::class, 20)->create();
-        factory(App\Category::class, 5)->create();
-        factory(App\Comment::class, 40)->create();
+        // factory(App\Article::class, 20)->create();
+        // factory(App\Category::class, 5)->create();
+        // factory(App\Comment::class, 40)->create();
 
         factory(App\User::class)->create([
             "name" => "Alice",
